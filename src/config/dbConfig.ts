cc74ea1e2bc3ec8f3eb0aae2d5a827f68da736c0
Kamal Preet Singh <kamalpreetsingh025@gmail.com>
2024-04-06 11:42:59 +0530

    Mongo collection name change

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL!);
+    await mongoose.connect(process.env.MONGODB_URL!, {
+      dbName: "creartivity",
+      bufferCommands: false,
+    });
 
     console.log("Connected to MongoDB");
   } catch (error) {
